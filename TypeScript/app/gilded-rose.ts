4b104dcfb83989a9836166ce70d805e42559ac6f
Ravi K <ravi@daybreakhealth.com>
2026-02-20 12:02:22 +0530

    Refactor `GildedRose` quality update logic by extracting item-specific handlers.

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -38,41 +38,53 @@ export class GildedRose {
     this.items = items;
   }
 
-  private changeQuality(item: Item) {
-    if (item.name == ItemType.SULFURAS) {
-      item.quality < 50 && item.changeQuality(ItemOperation.Increment, 0);
-      return;
-    }
-
-    if (item.name == ItemType.BACKSTAGE) {
-      if (item.quality < 50) {
-        const limit = 50 - item.quality;
-
-        let increaseQualityBy = 1;
-        if (item.sellIn < 11) increaseQualityBy++;
-        if (item.sellIn < 6) increaseQualityBy++;
-        increaseQualityBy = Math.min(increaseQualityBy, limit);
-
-        item.changeQuality(ItemOperation.Increment, increaseQualityBy);
-      }
-
+  private handleAgedBrie(item: Item) {
+    if (item.quality < 50) {
+      item.changeQuality(ItemOperation.Increment, 1);
       if (item.sellIn < 0) {
-        item.quality = 0;
-      }
-      return;
-    }
-
-    if (item.name == ItemType.AGED_BRIE) {
-      if (item.quality < 50) {
         item.changeQuality(ItemOperation.Increment, 1);
-        if (item.sellIn < 0) {
-          item.changeQuality(ItemOperation.Increment, 1);
-        }
       }
-      return;
+    }
+  }
+
+  private handleBackstage(item: Item) {
+    if (item.quality < 50) {
+      const limit = 50 - item.quality;
+
+      let increaseQualityBy = 1;
+      if (item.sellIn < 11) increaseQualityBy++;
+      if (item.sellIn < 6) increaseQualityBy++;
+      increaseQualityBy = Math.min(increaseQualityBy, limit);
+
+      item.changeQuality(ItemOperation.Increment, increaseQualityBy);
     }
 
-    this.handleGenericItem(item)
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    }
+  }
+
+  private handleSulfuras(item: Item) {
+    return;
+  }
+
+  private changeQuality(item: Item) {
+    switch (item.name) {
+      case ItemType.AGED_BRIE:
+        this.handleAgedBrie(item);
+        break;
+
+      case ItemType.BACKSTAGE:
+        this.handleBackstage(item);
+        break;
+
+      case ItemType.SULFURAS:
+        this.handleSulfuras(item);
+        break;
+
+      default:
+        this.handleGenericItem(item);
+    }
   }
 
   private changeSellIn(item: Item) {
